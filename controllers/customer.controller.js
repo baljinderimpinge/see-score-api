3e2c3e9aed5c4892d41ce683690182bacefa7211
kuldeepgill.impinge <kuldeepgill@impingeonline.com>
2024-01-25 14:58:27 +0530

    loginuserdata

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -84,38 +84,38 @@ const login = async (req, res) => {
         console.log(publickey, "publickeypublickey")
         const decoded = await auth.jwtAuthVerify(token, publickey);
         console.log(decoded, "00--");
-        const existingUser = await userModel.findOne({
-            where: {
+        // const existingUser = await userModel.findOne({
+        //     where: {
 
-                email: decoded.email,
-                role: common.constant.CONSTANTS.CUSTOMER,
-                isDeleted: false
+        //         email: decoded.email,
+        //         role: common.constant.CONSTANTS.CUSTOMER,
+        //         isDeleted: false
 
-            },
-        })
-        if (!existingUser) {
-            console.log(decoded,"decoded")
-            const userPayload = {
-                email: decoded.email,
-                firstName: decoded.name,
-                lastName: decoded.lastname || null,
-                isEmailVerified: decoded.email_verified || null,
-                password: "1234",
-                picture: decoded.picture,
-                creationTs: Date.now()
-            };
-           // const result = await userModel.create(userPayload)
+        //     },
+        // })
+        // if (!existingUser) {
+        //     console.log(decoded,"decoded")
+        //     const userPayload = {
+        //         email: decoded.email,
+        //         firstName: decoded.name,
+        //         lastName: decoded.lastname || null,
+        //         isEmailVerified: decoded.email_verified || null,
+        //         password: "1234",
+        //         picture: decoded.picture,
+        //         creationTs: Date.now()
+        //     };
+        //     return res.status(200).json({
+        //         message: "Login successfully",
+        //         data: existingUser,
+        //         status: 200
+        //     });
+       // } 
             return res.status(200).json({
                 message: "Login successfully",
-                data: existingUser,
+                data: decoded,
                 status: 200
             });
-        } else {
-            return res.status(200).json({
-                message: "Login successfully",
-                status: 200
-            });
-        }
+       
     } catch (error) {
         console.log(error)
         return res.status(500).json({
@@ -144,41 +144,6 @@ const getAllThirdData = async (req, res) => {
     }
 };
 
-
-
-// const getToken = async (req, res) => {
-//     console.log(process.env.CLIENTSECRET,"process.env.CLIENTSECRET")
-//         const tenantId = req.body.tenatId;
-//         const clientId = process.env.CLIENTID;
-//         const clientSecret = process.env.CLIENTSECRET;
-//         const scope = process.env.SCOPE; // e.g., 'https://graph.microsoft.com/.default'
-//         const grantType = process.env.GRANT_TYPE;
-//         try {
-//           const response = await axios.post(
-//             `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`,
-//             `client_id=${clientId}&client_secret=${clientSecret}&scope=${scope}&grant_type=${grantType}`,
-//             {
-//               headers: {
-//                 'Content-Type': 'application/x-www-form-urlencoded',
-//               },
-//             }
-//           );
-
-//           // Access the token from the response
-//           const accessToken = response.data.access_token;
-//           console.log('Access Token:', accessToken);
-//           return accessToken;
-//         }
-//     catch (error) {
-//         console.log(error,"000")
-//         return res.status(500).json({
-//             message: "Internal server error!,", error,
-//             status: 500,
-
-//         })
-//     }
-
-// }
 const getToken = async (req, res) => {
     const tenantId = req.body.tenatId;
     const clientId = process.env.CLIENTID;
@@ -203,7 +168,6 @@ const getToken = async (req, res) => {
             }
         );
 
-        // Access the token from the response
         const accessToken = response.data.access_token;
         console.log('Access Token:', accessToken);
         if (accessToken) {
@@ -216,23 +180,14 @@ const getToken = async (req, res) => {
                     },
                 }
             );
-            console.log(newapi, "--0-0-0-")
-            let identity = newapi.data.value;
-            let identityScore = 0 ;
-            for(let i=0;i<identity.length;i++){
-                let tenant = identity[i];
-                if (tenant.azureTenantId === req.body.tenatId) {
-                    let score = tenant.averageComparativeScores;
-                    for(let j=0;j<score.length;j++){
+           // console.log(newapi, "--0-0-0-")
+            let identity = newapi.data.value[0].controlScores;
+            const activeObjects = identity.filter(obj => obj.controlName === 'UserRiskPolicy');
 
-                    }
-                    break; 
-                }
-
-            }
+           
             return res.status(200).json({
                 message: "Data fetched successfully",
-                data: identityScores,
+                data: activeObjects,
                 status: 200
             });
         }
@@ -289,16 +244,46 @@ const getRecomendations = async (req, res) => {
             console.log(newapi, "--0-0-0-")
             let activestatus = newapi.data.value;
             const activeObjects = activestatus.filter(obj => obj.status === 'active');
-            console.log(activeObjects,"activeObjectsactiveObjectsactiveObjects")
+    
+        if(req.query.id){
+            let checkid = req.query.id;
+            let datasend = activeObjects.filter(obj => obj.id === checkid)
+            const filteredData = datasend.map(obj => ({
+                id: obj.id,
+                displayName: obj.displayName,
+                priority: obj.priority,
+                insights: obj.insights,
+                benefits: obj.benefits,
+                actionSteps: obj.actionSteps,
+                actionURL: obj.actionURL
+
             
+        }));
             return res.status(200).json({
                 message: "Data fetched successfully",
-               data: activeObjects,
+               data: filteredData,
                 status: 200
             });
-        }
-        //return accessToken;
-    } catch (error) {
+    }
+        else{
+           // let data = activeObjects;
+            const filteredData = activeObjects.map(obj => ({
+                    id: obj.id,
+                    displayName: obj.displayName,
+                    priority: obj.priority,
+
+                
+            }));
+            return res.status(200).json({
+                message: "Data fetched successfully",
+               data: filteredData,
+                status: 200
+            });
+        
+    }
+} 
+    
+ } catch (error) {
         console.log(error, "000");
         return res.status(500).json({
             message: "Internal server error!",

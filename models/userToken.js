20b52c20d251b462201704dfb5cabca13012cd51
kuldeepgill.impinge <kuldeepgill@impingeonline.com>
2024-02-08 18:43:40 +0530

    cronjob

--- models/userToken.js
+++ models/userToken.js
@@ -14,6 +14,18 @@ const UserToken = db.sequelize.define("userToken", {
         type: Sequelize.BOOLEAN,
         defaultValue:false
     },
+    expires_in:{
+        type: Sequelize.DATE, // Specifies a date/time
+        defaultValue: null
+    },
+    refresh_token:{
+        type: Sequelize.STRING(500000), // Adjust the length as needed
+        defaultValue: null
+    },
+    tokentimestamp:{
+        type: Sequelize.DATE, // Specifies a date/time
+        defaultValue: null
+    },
     token:{
         type: Sequelize.STRING(500000), // Adjust the length as needed
         defaultValue: null

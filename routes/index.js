c5b5bcbb672241f34eb3394bbd306ed1d92e1a91
balvinderimpinge <balvinder.impinge@gmail.com>
2024-02-27 11:57:58 +0530

    security error

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,14 @@ const admin = require("./admin.route")
 router.use("/user",customerRouter)
 router.use("/admin",admin)
 
-router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: 'MSAL Node & Express Web App',
-        isAuthenticated: req.session.isAuthenticated,
-        username: req.session.account?.username,
-    });
-});
-
+// router.get('/', function (req, res, next) {
+//     res.send('<h1>Shivani</h1>', {
+//         title: 'MSAL Node & Express Web App',
+//         // isAuthenticated: req.session.isAuthenticated,
+//         // username: req.session.account?.username,
+//     });
+// });
+// router.get('/', function (req, res, next) {
+//     res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>MSAL Node & Express Web App</title></head><body><h1>Shivani</h1></body></html>');
+// });
 module.exports = router
